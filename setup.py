ca88d25d40107879161add6a7b5c792ecc35200e
noamprinz <noamprinz@campus.technion.ac.il>
2021-08-12 19:23:44 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='noam_utils',
+    version='1.0.0',
+    install_requires=requirements,
+    author='Noam Prinz',
+    author_email='noam443@gmail.com',
+    packages=['noam_utils'],
+)
